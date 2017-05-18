d484db66f3b465374ab3ca69b93be05bba85e16b
Jinksi <ericjinks@gmail.com>
2017-05-18 21:34:51 +1000

    Tonalities

--- index.js
+++ index.js
@@ -12,8 +12,18 @@ const app = createLoop(canvas, {
 
 addEventListeners()
 
+const tonalities = [
+  {
+    notes: ['C3', 'C4', 'G5'],
+    color: 'tomato'
+  },
+  {
+    notes: ['G3', 'G4', 'E5'],
+    color: 'turquoise'
+  }
+]
+
 let time = 0
-let opacity = 1
 let ease = 0.1
 let mouse = {
   x: 0,
@@ -25,11 +35,16 @@ let desired = {
 }
 let thing = {
   x: 0,
-  y: 0
+  y: 0,
+  color: tonalities[0].color
 }
 let filter
 let dist
 let poly
+
+function selectTone () {
+  return tonalities[Math.floor(Math.random() * tonalities.length)]
+}
 
 StartAudioContext(Tone.context, '#start-button')
   .then(function(){
@@ -38,7 +53,13 @@ StartAudioContext(Tone.context, '#start-button')
     dist = new Tone.Distortion(1)
     poly = new Tone.PolySynth(3, Tone.Synth)
       .chain(dist, filter, Tone.Master)
-      .triggerAttack(["C3", "C4", "G5"], undefined, 0.8);
+      .triggerAttack(tonalities[0].notes, 0, 0.8)
+    Tone.Transport.start()
+    Tone.Transport.scheduleRepeat(function(time){
+      const tonality = selectTone()
+    	poly.releaseAll().triggerAttack(tonality.notes, time, 0.8)
+      thing.color = tonality.color
+    }, '1m', '1m')
     app.start()
   })
 
@@ -55,7 +76,7 @@ app.on('tick', (dt) => {
   context.fillStyle = `rgba(0,0,0,${0.1})`
   context.scale(app.scale, app.scale)
   context.fillRect(0, 0, w, h)
-  context.fillStyle = `rgba(255,255,255,${opacity})`
+  context.fillStyle = thing.color
   context.translate((w / 2) - (thing.x * w), (h / 2) - (thing.y * h))
   context.scale(thing.x * 2, thing.y * 2)
   context.fillRect(0, 0, w, h)
